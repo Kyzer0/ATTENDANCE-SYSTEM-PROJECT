5c8e1f399cd1ab6d1bc5cdaad524504d448ae3d3
Jervie Gono <112779985+Kyzer0@users.noreply.github.com>
2024-10-31 10:34:48 +0800

    Initialize ClassSystemBrance

---
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -0,0 +1,59 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template 
+from FlaskWebProject1 import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'layout.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/teachersLogin')
+def teachersLogin():
+    """Renders the contact page."""
+    app.logger.info('Navigating to teachersLogin page')
+    return render_template(
+        'teachersLogin.html',
+        title='Teachers Account',
+        year=datetime.now().year,
+        message='Account page.'
+    )
+
+
+@app.route('/attendancePage')
+def attendancePage():
+    """Renders the attendance Page"""
+    return render_template(
+        'attendancePage.html',
+        title='Attendance',
+        year=datetime.now().year,
+        message='Attendance Domain'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/sectionAdding')
+def sectionAdding():
+    """Renders the about page."""
+    return render_template(
+        'sectionAdding.html',
+        title='Section Adding',
+        year=datetime.now().year,
+        message='Add a section.'
+    )
